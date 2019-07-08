0741f18491bdeeae7fef1415204bf74bf2a3b26b
Michael Shepanski <mikes@nasuni.com>
2019-07-08 16:29:24 -0400

    Remove duplicate order by

--- jira.1c.300s+.py
+++ jira.1c.300s+.py
@@ -20,7 +20,7 @@ CACHEFILE = os.path.join(os.path.dirname(__file__), CACHENAME)
 cache = {}  # global cache object
 
 ASSIGNED_ISSUES = 'assignee = currentUser() AND statusCategory != done ORDER BY issueType'
-RECENT_ISSUES = 'issuekey in issueHistory() ORDER BY lastViewed DESC ORDER BY issueType'
+RECENT_ISSUES = 'issuekey in issueHistory() ORDER BY lastViewed DESC'
 
 
 def _get_jira_auth():
